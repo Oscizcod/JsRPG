0a60116ec8cf2aad0b795fabb9e5a7121fdf3528
Pravin <pravin_muruhaya@hotmail.com>
2022-12-06 11:57:46 +0800

    Add "fight" element to locations array

--- script.js
+++ script.js
@@ -72,6 +72,12 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
+    },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
     }
 ];
 
@@ -154,13 +160,20 @@ function sellWeapon() {
 
 
 function fightDragon() {
-    console.log("Fighting dragon.");
+    fighting = 2;
+    goFight();
 }
 
 function fightSlime() {
-
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast() {
+    fighting = 1;
+    goFight();
+}
+
+function goFight() {
 
 }
